4cabb0ed67c3cde7dd744b5a7db853349d71e474
maritoo17 <msancgim@myuax.com>
2023-11-30 12:30:01 +0100

    Cuarta parte del ejercicio en la que añado la función "load_script" que será la encargada de abrir y leer los archivos que el usuario pida. A parte, incluimos los dos errores que se nos pueden dar a la hora de intentar abrir y leer los archivos.

--- main.cpp
+++ main.cpp
@@ -19,5 +19,48 @@ struct ConsoleBox
     void set_text(const string &text) { cout << text << endl; }
 };
 
-ConsoleBox *consoleBox = new ConsoleBox; // suponemos que ya está inicializado
+ConsoleBox *consoleBox = new ConsoleBox;
+
+void load_script(const char* filename, bool show_script = false)
+{
+    string script;
+    FILE* f = nullptr;
+    try
+    {
+        f = fopen(filename, "rb");
+        if (!f)
+        {
+            cerr << "Error al abrir " << filename << endl;
+            return;
+        }
+
+        char buf[4001];
+        size_t bytes_read = fread(buf, 1, 4000, f);
+        while (bytes_read > 0)
+        {
+            buf[bytes_read] = '\0'; // Null-terminate the buffer
+            script.append(buf, bytes_read);
+            bytes_read = fread(buf, 1, 4000, f);
+        }
+
+        fclose(f);
+        f = nullptr;
+
+        if (show_script)
+        {
+            cout << ColorConsole::fg_red << ColorConsole::fg_black << endl;
+            cout << script << endl;
+        }
+        consoleBox->new_text();
+        consoleBox->set_text(script);
+    }
+    catch (const std::exception& e)
+    {
+        cerr << "Error durante la lectura del archivo: " << e.what() << endl;
+        if (f)
+            fclose(f);
+    }
+}
+
+
 
