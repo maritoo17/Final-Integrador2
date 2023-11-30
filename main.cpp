71889c62fdecc417e315e4d21197c4ca81b8be3e
maritoo17 <msancgim@myuax.com>
2023-11-30 12:27:08 +0100

    Tercera parte del ejercicio en la que creo la estructura ConsoleBox que tiene integrada dos tipos de funciones distintas, en este caso, new text y set text que será el encargado de imprimir el texto.

--- main.cpp
+++ main.cpp
@@ -13,3 +13,11 @@ struct ColorConsole
 const char* ColorConsole::fg_red = "\033[31m";
 const char* ColorConsole::fg_black = "\033[30m";
 
+struct ConsoleBox
+{
+    void new_text() {/*...*/}
+    void set_text(const string &text) { cout << text << endl; }
+};
+
+ConsoleBox *consoleBox = new ConsoleBox; // suponemos que ya está inicializado
+
